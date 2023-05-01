b55aebdea8abb60b4a307dfc45a68bfb7bfe1c26
Ruperto A. Martinez <77865052+RupertoM@users.noreply.github.com>
2023-04-30 22:46:02 -0400

    whitespace changes

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ fs.readFile('./public/index.html', 'utf-8', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
-
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
 // The function should have three responses: 
